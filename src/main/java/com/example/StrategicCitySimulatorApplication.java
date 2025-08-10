17c2682fedc3f7ad79cab43fb87a51654f1816cd
lee,jaeil <sonegy@gmail.com>
2025-08-11 00:42:59 +0900

    chore: add Gradle Spring Boot bootstrap

---
+++ src/main/java/com/example/StrategicCitySimulatorApplication.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrategicCitySimulatorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StrategicCitySimulatorApplication.class, args);
+    }
+}
