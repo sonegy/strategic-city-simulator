83d80963a836eff8bef76ec7d98529f0dcf1f7f3
lee,jaeil <sonegy@gmail.com>
2025-08-11 12:32:00 +0900

    test: verify profile switching

---
+++ src/test/java/com/example/LocalProfileTests.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest(properties = {
+    "spring.datasource.url=jdbc:h2:mem:localtest;DB_CLOSE_DELAY=-1",
+    "spring.datasource.driverClassName=org.h2.Driver",
+    "spring.datasource.username=sa",
+    "spring.datasource.password=",
+    "spring.jpa.hibernate.ddl-auto=create-drop"
+})
+@ActiveProfiles("local")
+class LocalProfileTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
